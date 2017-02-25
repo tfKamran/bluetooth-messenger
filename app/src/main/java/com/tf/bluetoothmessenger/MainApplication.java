c6890bcf9175e92356f833af6a1d5449214bcded
Kamran Ahmed Ansari <team.radiant@gmail.com>
2017-02-25 16:42:49 +0530

    Add MainApplication as the application class

---
+++ app/src/main/java/com/tf/bluetoothmessenger/MainApplication.java
@@ -0,0 +1,15 @@
+package com.tf.bluetoothmessenger;
+
+import android.app.Application;
+
+/**
+ * Created by kamran on 25/02/17.
+ */
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/tf/bluetoothmessenger/view/MainActivity.java
+++ app/src/main/java/com/tf/bluetoothmessenger/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.tf.bluetoothmessenger;
+package com.tf.bluetoothmessenger.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tf.bluetoothmessenger.R;
 
 public class MainActivity extends AppCompatActivity {
 
