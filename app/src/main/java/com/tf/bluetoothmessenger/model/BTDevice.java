bf3a2317f156def06878d3764d010d2634e9ec7b
Kamran Ahmed Ansari <team.radiant@gmail.com>
2017-02-26 00:01:21 +0530

    Make BTDevice Parcelable

--- app/src/main/java/com/tf/bluetoothmessenger/model/BTDevice.java
+++ app/src/main/java/com/tf/bluetoothmessenger/model/BTDevice.java
@@ -1,6 +1,8 @@
 package com.tf.bluetoothmessenger.model;
 
 import android.bluetooth.BluetoothDevice;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import io.palaima.smoothbluetooth.Device;
 
@@ -8,7 +10,7 @@ import io.palaima.smoothbluetooth.Device;
  * Created by kamran on 25/02/17.
  */
 
-public class BTDevice {
+public class BTDevice implements Parcelable {
 
     private String address;
     private String name;
@@ -46,4 +48,32 @@ public class BTDevice {
     public boolean equals(Object obj) {
         return address.equals(((BTDevice) obj).getAddress());
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.address);
+        dest.writeString(this.name);
+    }
+
+    protected BTDevice(Parcel in) {
+        this.address = in.readString();
+        this.name = in.readString();
+    }
+
+    public static final Parcelable.Creator<BTDevice> CREATOR = new Parcelable.Creator<BTDevice>() {
+        @Override
+        public BTDevice createFromParcel(Parcel source) {
+            return new BTDevice(source);
+        }
+
+        @Override
+        public BTDevice[] newArray(int size) {
+            return new BTDevice[size];
+        }
+    };
 }
